69b9d7556b1f8715115d13f452398de4e9ad3a67
20220439 <youssef.elmohamadi.dev@gmail.com>
2026-08-14 00:29:20 +0300

    v1.0

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   eslint: { ignoreDuringBuilds: true },
+  typescript: { ignoreBuildErrors: true },
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
